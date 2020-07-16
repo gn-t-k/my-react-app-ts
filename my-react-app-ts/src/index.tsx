3a4254eb1915182da5ea01a03a9b631885ba25c1
gn-t-k <gntk1995422@gmail.com>
2020-07-16 23:24:56 +0900

    implement state liftup

--- my-react-app-ts/src/index.tsx
+++ my-react-app-ts/src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 interface SquarePropsInterface {
-  value: number;
+  value: string;
+  onClick: () => void;
 }
 
 interface SquareStateInterface {
@@ -14,27 +15,44 @@ class Square extends React.Component<
   SquarePropsInterface,
   SquareStateInterface
 > {
-  constructor(props: SquarePropsInterface) {
-    super(props);
-    this.state = {
-      value: '',
-    };
-  }
   render(): JSX.Element {
     return (
-      <button
-        className="square"
-        onClick={(): void => this.setState({ value: 'X' })}
-      >
-        {this.state.value}
+      <button className="square" onClick={(): void => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
-class Board extends React.Component {
+interface BoardPropsInterface {
+  squares: string[];
+}
+
+interface BoardStateInterface {
+  squares: string[];
+}
+
+class Board extends React.Component<BoardPropsInterface, BoardStateInterface> {
+  constructor(props: BoardPropsInterface) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(''),
+    };
+  }
+
+  handleClick(i: number): void {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i: number): JSX.Element {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={(): void => this.handleClick(i)}
+      />
+    );
   }
 
   render(): JSX.Element {
@@ -68,7 +86,7 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board squares={Array(9).fill('')} />
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
