9f1f329fe1218ecc3ddeb12ff72bc36caf90b462
gn-t-k <gntk1995422@gmail.com>
2020-07-25 00:07:49 +0900

    boardを関数コンポーネント化

--- my-react-app-ts/src/components/board.tsx
+++ my-react-app-ts/src/components/board.tsx
@@ -6,42 +6,30 @@ interface BoardPropsInterface {
   onClick: (i: number) => void;
 }
 
-interface BoardStateInterface {
-  squares: string[];
-  xIsNext: boolean;
-}
+const Board: React.FC<BoardPropsInterface> = (props) => {
+  const renderSquare = (i: number): JSX.Element => (
+    <Square value={props.squares[i]} onClick={(): void => props.onClick(i)} />
+  );
 
-class Board extends React.Component<BoardPropsInterface, BoardStateInterface> {
-  renderSquare(i: number): JSX.Element {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={(): void => this.props.onClick(i)}
-      />
-    );
-  }
-
-  render(): JSX.Element {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
-}
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
 
 export default Board;
